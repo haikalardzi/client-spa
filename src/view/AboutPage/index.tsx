35d8dd259465affad6435c76b76f8582103725bd
christodharma <13521009@std.stei.itb.ac.id>
2023-11-14 19:18:25 +0700

    feat: about page feat: left-to-right wipe reveal style feat: about page content feat: tailwind config

--- src/view/AboutPage/index.tsx
+++ src/view/AboutPage/index.tsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import logo from '../../components/images/logo.svg'
-// import styles from './AboutPage.module.css'
+import "../../components/fragments/L2RWipeReveal/L2RWipeReveal.css"
 
 function AboutPageView() {
     return (
         <div className = 
-        "flex flex-column flex-nowrap justify-center items-center ">
+        "flex flex-col flex-nowrap justify-center items-center text-tertiary text-center py-5">
             <title>
                 About Saranghaengbok
             </title>
-            <div>
+            <div 
+            className="p-12 bg-primary self-stretch flex justify-center L2RWipeRevealElement mb-24">
                 <img 
                 src={logo}
-
+                className="w-100 h-100"
                 ></img>
             </div>
             <div 
-            className="flex flex-column flex-nowrap justify-start text-left
+            className="flex flex-col flex-nowrap justify-start text-left mx-auto px-6 max-w-screen-lg
             ">
-                <h1 className="p-15">
+                <h1 className="py-5">
                     About Saranghaengbok
                 </h1>
-                <p className ="p-15">
-                Dari tahun 2023, Saranghaengbok adalah aplikasi sarana bagi penggemar K-POP yang ingin membeli merch K-POP band favoritnya dengan memberikan hasil pencarian merch K-POP yang sedang dibuka untuk group ordering maupun individual ordering, menghubungkan pembeli dengan penjual, dan menyediakan wadah untuk transaksi dari penjual dan pembeli. Pada awalnya, Saranghaengbok dikembangkan untuk menyelesaikan suatu masalah yang dihadapi oleh penggemar K-POP yang kesulitan dalam mencari merch K-POP karena informasi penjualannya yang terpisah-pisah dengan mulut ke mulut atau informasi yang tidak diregulasi, transaksi yang tidak aman atau tidak terpercaya, dan media komunikasi yang sulit karena tembok bahasa atau media yang sulit. Karena hal ini, Christo, Haikal, dan Varraz mengembangkan mengembangkan cara untuk menengahi hal ini sehingga masalah yang dihadapi bisa dipermudah.
+                <p className ="py-5">
+                Dari tahun 2023, Saranghaengbok adalah aplikasi sarana bagi penggemar K-POP yang ingin membeli merch K-POP band favoritnya dengan memberikan hasil pencarian merch K-POP yang sedang dibuka untuk group ordering maupun individual ordering, menghubungkan pembeli dengan penjual, dan menyediakan wadah untuk transaksi dari penjual dan pembeli. Pada awalnya, Saranghaengbok dikembangkan untuk menyelesaikan suatu masalah yang dihadapi oleh penggemar K-POP yang kesulitan dalam mencari merch K-POP karena informasi penjualannya yang terpisah-pisah dengan mulut ke mulut atau informasi yang tidak diregulasi, transaksi yang tidak aman atau tidak terpercaya, dan media komunikasi yang sulit karena tembok bahasa atau media yang sulit. Karena hal ini, Christo, Haikal, dan Varraz mengembangkan cara untuk menengahi hal ini sehingga bisa mempermudah masalah yang dihadapi.
                 </p>
             </div>
-            {/* TODO: Home Page details */}
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        primary: '#AA1E1E',
+        secondary: '#3c00a0',
+        tertiary: '#fefefe',
+        quaternary: '#2ec4b6',
+        quinary: '#20a4f3',
+        whitesmoke: '#f5f5f5'
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
